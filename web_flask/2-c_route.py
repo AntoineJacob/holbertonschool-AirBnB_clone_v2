88148d161d70f907ec5bf509218d16b2a094077e
AntoineJacob <5646@holbertonstudents.com>
2023-04-04 04:40:51 -0700

    adding the c_route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" 2 C_route """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """ display HBNB. """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """display “C ” followed by the value of the text variable (replace underscore _ symbols with a space )"""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
