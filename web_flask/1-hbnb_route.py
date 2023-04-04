a5b2d8bc07ae16ddca5759a70bd41799ed7c546e
AntoineJacob <5646@holbertonstudents.com>
2023-04-04 04:31:29 -0700

    adding display hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 1 HBNB route """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """display HBNB"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
