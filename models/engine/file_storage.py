2c3451873e791aa3d95b3281cada6dbea6398459
AntoineJacob <5646@holbertonstudents.com>
2023-04-06 04:33:49 -0700

    fixing pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,6 +63,6 @@ class FileStorage:
             del self.__objects[key]
 
 
-     def close(self):
+    def close(self):
         """ calls reload() for deserializing the JSON file to objects."""
         self.reload()
